ef4b23be13aaa220cc4ef9525cf8d1ae689ef857
J Silverstein <jess.pebble@gmail.com>
2017-12-19 22:37:21 -0500

    include tests for accordion component

---
+++ __test__/Accordion.test.js
@@ -0,0 +1,83 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { shallow, mount } from "enzyme";
+
+import { singlePane, multiPane } from "./sampleComponents";
+
+describe("Single pane accordion", () => {
+  it("Renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(singlePane, div);
+  });
+  it("displays the panes it is given", () => {
+    const wrapper = shallow(singlePane);
+    expect(wrapper.find("Pane")).toHaveLength(3);
+  });
+  it("expands one pane at a time", () => {
+    const wrapper = mount(singlePane);
+    wrapper.find("button").forEach(button => {
+      button.simulate("click");
+    });
+    expect(wrapper.find("h2")).toHaveLength(1);
+  });
+  it("changes panes when each pane is clicked", () => {
+    const wrapper = mount(singlePane);
+    wrapper
+      .find("button")
+      .first()
+      .simulate("click");
+    const firstPane = wrapper.find("h2").text();
+    wrapper
+      .find("button")
+      .last()
+      .simulate("click");
+    expect(firstPane).not.toBe(wrapper.find("h2").text());
+  });
+  it("displays the active pane's content", () => {
+    const wrapper = mount(singlePane);
+    wrapper
+      .find("button")
+      .first()
+      .simulate("click");
+    expect(wrapper.find("h2").text()).toEqual("First Pane");
+    wrapper
+      .find("button")
+      .last()
+      .simulate("click");
+    expect(wrapper.find("h2").text()).toEqual("Third Pane");
+  });
+});
+
+describe("Multi pane accordion", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(multiPane, div);
+  });
+  it("expands more than one pane at a time", () => {
+    const wrapper = mount(multiPane);
+    wrapper.find("button").forEach(button => {
+      button.simulate("click");
+    });
+    expect(wrapper.find("h2")).toHaveLength(3);
+  });
+  it("closes each pane on click", () => {
+    const wrapper = mount(multiPane);
+    wrapper.find("button").forEach(button => {
+      button.simulate("click");
+    });
+    wrapper.find("button").forEach(button => {
+      button.simulate("click");
+    });
+    expect(wrapper.find("h2")).toHaveLength(0);
+  });
+  it("displays each active pane's content", () => {
+    const wrapper = mount(multiPane);
+    wrapper.find("button").forEach(button => {
+      button.simulate("click");
+    });
+    const titles = ["First Pane", "Second Pane", "Third Pane"];
+    wrapper.find("h2").forEach((h2, index) => {
+      expect(h2.text()).toEqual(titles[index]);
+    });
+  });
+});
