efc58089be560b6fdf4a43ac63d08ca0656e8cfa
J Silverstein <jess.pebble@gmail.com>
2017-12-19 22:14:33 -0500

    add sample component store to avoid extra-long tests

---
+++ __test__/sampleComponents.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+import { Accordion } from "../src/Accordion";
+import { Pane } from "../src/Pane";
+
+export const singlePane = (
+  <Accordion isMultiPane={false}>
+    <Pane title="Pane 1">
+      <h1>Here is my first accordion pane</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ante
+        metus, dignissim eu sapien vitae, finibus tristique arcu. Quisque
+        sollicitudin velit a purus venenatis venenatis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ante
+        metus, dignissim eu sapien vitae, finibus tristique arcu. Quisque
+        sollicitudin velit a purus venenatis venenatis.
+      </p>
+    </Pane>
+    <Pane title="Pane 2">
+      <h2>Second Pane</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ante
+        metus, dignissim eu sapien vitae, finibus tristique arcu. Quisque
+        sollicitudin velit a purus venenatis venenatis.
+      </p>
+    </Pane>
+    <Pane title="Pane 3">
+      <h2>Third Pane</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ante
+        metus, dignissim eu sapien vitae, finibus tristique arcu. Quisque
+        sollicitudin velit a purus venenatis venenatis.
+      </p>
+    </Pane>
+  </Accordion>
+);
+
+export const multiPane = (
+  <Accordion isMultiPane={true}>
+    <Pane title="Pane 1">
+      <h1>Here is my first accordion pane</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ante
+        metus, dignissim eu sapien vitae, finibus tristique arcu. Quisque
+        sollicitudin velit a purus venenatis venenatis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ante
+        metus, dignissim eu sapien vitae, finibus tristique arcu. Quisque
+        sollicitudin velit a purus venenatis venenatis.
+      </p>
+    </Pane>
+    <Pane title="Pane 2">
+      <h2>Second Pane</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ante
+        metus, dignissim eu sapien vitae, finibus tristique arcu. Quisque
+        sollicitudin velit a purus venenatis venenatis.
+      </p>
+    </Pane>
+    <Pane title="Pane 3">
+      <h2>Third Pane</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ante
+        metus, dignissim eu sapien vitae, finibus tristique arcu. Quisque
+        sollicitudin velit a purus venenatis venenatis.
+      </p>
+    </Pane>
+  </Accordion>
+);
